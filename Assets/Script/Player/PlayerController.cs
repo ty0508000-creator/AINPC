e90799bb0210a7b3c07c3dca07a9a4459dc9d1ca
Jungtaek Lee <bluepet0302@gmail.com>
2026-03-29 03:39:56 +0900

    1
    1

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -4,23 +4,25 @@ using UnityEngine.InputSystem;
 public class Player_Controller : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
     private Vector2 moveInput;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        // Input System을 통해 WASD 입력 받기 (신규방식)
         moveInput = InputSystem.actions.FindAction("Move").ReadValue<Vector2>();
+        
+        // 애니메이션 속도 설정
         animator.SetFloat("Speed", moveInput.magnitude);
 
+        // 좌우 스프라이트 반전
         if (moveInput.x < 0)
             spriteRenderer.flipX = true;
         else if (moveInput.x > 0)
@@ -29,6 +31,8 @@ public class Player_Controller : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = moveInput.normalized * moveSpeed;
+        // WASD로 캐릭터 이동
+        Vector2 moveDirection = moveInput.normalized;
+        transform.position += (Vector3)moveDirection * moveSpeed * Time.fixedDeltaTime;
     }
 }
